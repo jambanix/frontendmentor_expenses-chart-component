c0ef32d74070ebd3d3c22f8d16c866cb5e4462cd
jambanix <jambanix@pm.me>
2024-11-19 18:50:41 +0000

    make BUFFER const a function so this value can change from 100 depending on screensize as think this is where the bug is coming from as too much buffer reducing height by too much

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -4,11 +4,12 @@ import { useScreenWidth } from "../hooks/useScreenWidth"
 
 export const Chart = ({ data, maxAmount, containerHeight }) => {
 
-  const requiredResize = useScreenWidth() === "desktop" ? 0 : 100
+  const screenSize = useScreenWidth();
+  const requiredResize = screenSize === "desktop" ? 0 : 100
 
   return (
     <div className={`flex justify-evenly items-end gap-4 flex-grow`} style={{height: `${containerHeight}px`}}>
-      {data.map((entry, ix) => <Bar data={entry} key={ix} height={getBarHeight(entry.amount, maxAmount) - requiredResize}/>)}
+      {data.map((entry, ix) => <Bar data={entry} key={ix} height={getBarHeight(entry.amount, maxAmount, screenSize) - requiredResize}/>)}
     </div>
   )
 }
--- src/lib/container.js
+++ src/lib/container.js
@@ -3,13 +3,13 @@
 export const CHART_CONTAINER_HEIGHT = 400;
 
 // Leave some room for the pop up amount to stop the bar shrinking on hover
-const BUFFER = 100;
+const BUFFER = (screenSize="desktop") => screenSize === "desktop" ? 100 : 60
 
 
 // Calculate the height of the bar.
 // Highest amount uses full size then everything else is relative to that
-export const getBarHeight = (amount, maxAmount, containerHeight=CHART_CONTAINER_HEIGHT) => {
-  if (amount === maxAmount) return containerHeight - BUFFER
+export const getBarHeight = (amount, maxAmount, screensize, containerHeight=CHART_CONTAINER_HEIGHT) => {
+  if (amount === maxAmount) return containerHeight - BUFFER();
   const comparisonMultiplier = (amount / maxAmount);
-  return ((comparisonMultiplier * containerHeight) - BUFFER).toFixed(0);
+  return ((comparisonMultiplier * containerHeight) - BUFFER()).toFixed(0);
 }
